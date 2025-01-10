0bfd260b21a92a3958e2c4833382ddbc24db5708
luke.yang <luke.yang@belivvr.com>
2025-01-10 11:36:23 +0000

    Add support for optional environment file in build script

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 set -ex
 cd "$(dirname "$0")"
 
+# Source the environment file if needed
+ENV_FILE="../.env.cnu"
+if [ -f "$ENV_FILE" ]; then
+  source "$ENV_FILE"
+fi
+
 docker build --no-cache -t client .
